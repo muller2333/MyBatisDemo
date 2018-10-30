aa45c33e9ebca5dc044a296456bd2aadaf5dc976
muller_glb <muller_glb@gmail.com>
2018-10-30 21:40:59 +0800

    删除不需要的导入

--- src/main/java/com/muller/MyTest.java
+++ src/main/java/com/muller/MyTest.java
@@ -4,7 +4,6 @@ import com.muller.beans.Student;
 import com.muller.dao.IStudentDao;
 import com.muller.dao.impl.IStudentDaoImpl;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
